dc77db362a445afc8a44b061e19dc6822bc0117f
JangJinWoo <aktsksqkq@gmail.com>
2025-02-24 04:43:14 +0900

    IA, IMC 제작 및 매핑

---
+++ Source/SevenDays/Private/MyPlayerController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+
+AMyPlayerController::AMyPlayerController()
+{
+	InputMappingContext = nullptr;
+	MoveAction = nullptr;
+	LookAction = nullptr;
+	JumpAction = nullptr;
+	SprintAction = nullptr;
+	CrouchAction = nullptr;
+
+}
---
+++ Source/SevenDays/Public/MyPlayerController.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class SEVENDAYS_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AMyPlayerController();
+
+private:
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input|IMC")
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* SprintAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* CrouchAction;
+
+
+
+protected:
+};
