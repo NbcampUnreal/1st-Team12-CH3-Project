541eac7db668d5279a35aaa9b2277626f0b6b87f
JangJinWoo <aktsksqkq@gmail.com>
2025-03-06 22:40:42 +0900

    총기 수치 조정

--- Source/SevenDays/Private/TestGun/NBC_BaseGun.cpp
+++ Source/SevenDays/Private/TestGun/NBC_BaseGun.cpp
@@ -11,14 +11,14 @@
 UNBC_BaseGun::UNBC_BaseGun()
 {
 	//총기 관련 셋팅
-	FPistol.SetReloadDelay(2.0f)
-		.SetShotDelay(0.1f)
+	FPistol.SetReloadDelay(1.0f)
+		.SetShotDelay(0.2f)
 		.SetDamage(25)
 		.SetMaxRecoil(2.0f, 5.0f)
 		.SetBulletInfo(6, 30);
 
-	FRifle.SetReloadDelay(1.0f)
-		.SetShotDelay(2.0f)
+	FRifle.SetReloadDelay(2.0f)
+		.SetShotDelay(0.1f)
 		.SetDamage(20)
 		.SetMaxRecoil(1.5f, 4.0f)
 		.SetBulletInfo(30, 150);
